27eff49363476caf6d4b0d22ddddd669c4267ded
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2014-04-20 00:47:27 +0200

    Factorize send method.

--- dht.py
+++ dht.py
@@ -5,6 +5,8 @@ import random
 import socket
 from utils import hex2byte
 import logging
+from models import Node
+
 
 stream = logging.StreamHandler()
 formatter = logging.Formatter('%(levelname)s - %(filename)s:%(lineno)d - %(message)s')
@@ -18,20 +20,34 @@ def generate_random_id():
     # Generate a 160-bit (20-byte) random node ID.
     return ''.join([chr(random.randint(0, 255)) for _ in range(20)])
 
-def send_to_bootstrap_node(data):
+def send_to_node(node, data):
     data_bencoded = bencode.bencode(data)
 
     # Send a datagram to a server and recieve a response.
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.sendto(data_bencoded,
-             (socket.gethostbyname('router.bittorrent.com'), 6881))
+    s.sendto(data_bencoded, (node.ip, node.port))
 
-    r = s.recvfrom(1024)
+    r = None
+    try:
+        r = s.recvfrom(4096)
+    except socket.timeout:
+        logger.debug('timeout for %s' % node)
+        return r
 
-    return bencode.bdecode(r[0])
+    try:
+        # de-bencode
+        r = bencode.bdecode(r[0])
+    except bencode.BTL.BTFailure:
+        logger.error('response not bencoded: \'%s\'' % r[0])
+        return None
+
+    if r.has_key('r'):
+        return r['r']
+    else:
+        logger.error('error for query \'%s\'' % data)
+        return r
 
 def bootstrap(_id):
-
 # Create ping query and bencode it.
 # "'y': 'q'" is for "query".
 # "'t': '0f'" is a transaction ID which will be echoed in the response.
@@ -43,6 +59,9 @@ def bootstrap(_id):
                   'q': 'ping',
                   'a': {'id': _id}}
 
+    response = send_to_node(Node.get_boostrap_node(), ping_query)
+
+    print(response)
     get_peers_query = {
         'y': 'q',
         't': '0f',
@@ -53,9 +72,6 @@ def bootstrap(_id):
         }
     }
 
-    send_to_bootstrap_node(ping_query)
-
-    response = send_to_bootstrap_node(get_peers_query)
 
 
 
