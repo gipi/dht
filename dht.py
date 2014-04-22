e295addfa750647e672dcdb1491c5f90ce52fe8b
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2014-04-22 22:08:44 +0200

    Implement recursive search for info hash.

--- dht.py
+++ dht.py
@@ -6,6 +6,8 @@ import random
 import socket
 from utils import bt_nodes_info_from_raw_data, bt_contact_peer, hex2byte
 import logging
+from multiprocessing import Process, Pool
+
 from models import Node
 
 
@@ -64,6 +66,10 @@ class NodeResponse(Response):
     def __init__(self, nodes):
         self.nodes = nodes
 
+# www.rueckstiess.net/research/snippets/show/ca1d7d90
+def unwrap_self_f(arg, **kwarg):
+    return DHT.get_peers(*arg, **kwarg)
+
 class DHT(object):
     '''This represents the Distributed Hash Table with its own queries
     
@@ -116,6 +122,22 @@ class DHT(object):
     def announce_peer(self, _id, target):
         raise NotImplementedError('announce_peer is not implemented')
 
+    def get_recursively_peers(self, _id, nodes, info_hash):
+        logger.debug('asking for peers to nodes ' + str(nodes))
+        pool = Pool()
+        args = [(self, _id, x, info_hash) for x in nodes if x is not None]
+
+        # this returns a list of list
+        results = pool.map(unwrap_self_f, args)
+        results = [x for x in results if x is not None]
+        logger.info([str(x) for x in results])
+        for result in results:
+            self.get_recursively_peers(_id, result, info_hash)
+
+    def find_peers_for_infohash(self, _id, info_hash):
+        result = self.get_recursively_peers(_id, [Node.get_boostrap_node()], info_hash)
+
+
 if __name__ == "__main__":
     my_id = generate_random_id()
     print('id: ' + my_id.encode('hex'))
@@ -124,5 +146,5 @@ if __name__ == "__main__":
 
     info_hash = 'bbb6db69965af769f664b6636e7914f8735141b3' if len(sys.argv) == 1 else sys.argv[1]
 
-    peers_result = dht.get_peers(my_id, Node.get_boostrap_node(), info_hash)
-    logger.debug('results from bootstrap node: ' + str(peers_result))
+
+    dht.find_peers_for_infohash(my_id, info_hash)
